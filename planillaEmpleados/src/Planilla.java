92f94a6e9ab0f7e06b3da47f83e8c7f73bfa6e85
Polvos Magicos <73202603+PolvosMagicos@users.noreply.github.com>
2023-10-23 02:35:18 -0500

    feat: Interface display

--- planillaEmpleados/src/Planilla.java
+++ planillaEmpleados/src/Planilla.java
@@ -1,2 +1,21 @@
+import javax.swing.*;
+
 public class Planilla {
+    private JPanel panel1;
+    private JPanel agregarPanel;
+    private JPanel buscarMostrarPanel;
+    private JRadioButton agregarTrabajadorRadioButton;
+    private JRadioButton operacionesRadioButton;
+    private JRadioButton empleadoRadioButton;
+    private JRadioButton consultorRadioButton;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JPanel empleadoPanel;
+    private JPanel consultorPanel;
+    private JButton agregarButton;
+    private JTextField textField4;
+    private JTextField textField5;
+    private JTextField textField6;
+    private JTextField textField7;
 }
