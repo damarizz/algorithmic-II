0cdfbc132ece3b81db79a7a76f2c9d135a421aae
damarizz <138645520+damarizz@users.noreply.github.com>
2023-11-01 23:31:40 -0500

    feat: add and search panels in gui

--- ListasEnlazadas/src/InstitutoForm.java
+++ ListasEnlazadas/src/InstitutoForm.java
@@ -1,2 +1,25 @@
+import javax.swing.*;
+
 public class InstitutoForm {
+    private JPanel panel1;
+    private JRadioButton agregarRadioButton;
+    private JRadioButton modificarRadioButton;
+    private JRadioButton buscarRadioButton;
+    private JRadioButton eliminarRadioButton;
+    private JPanel AgregarPanel;
+    private JPanel ModificarPanel;
+    private JPanel BuscarPanel;
+    private JPanel EliminarPanel;
+    private JButton AGREGARButton;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JRadioButton alFinalRadioButton;
+    private JRadioButton posiciónRadioButton;
+    private JRadioButton alInicioRadioButton;
+    private JTextField textField3;
+    private JRadioButton porCódigoRadioButton;
+    private JRadioButton porNombreRadioButton;
+    private JTextField textField4;
+    private JTextField textField5;
+    private JButton BUSCARButton;
 }
