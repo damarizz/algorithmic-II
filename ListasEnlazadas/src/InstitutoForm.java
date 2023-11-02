65f84c3735f6451fa529da6c49a24d9c980fe0d5
damarizz <138645520+damarizz@users.noreply.github.com>
2023-11-02 16:48:17 -0500

    feat: finishing gui's structure

--- ListasEnlazadas/src/InstitutoForm.java
+++ ListasEnlazadas/src/InstitutoForm.java
@@ -1,6 +1,8 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class InstitutoForm {
+public class InstitutoForm extends JFrame {
     private JPanel panel1;
     private JRadioButton agregarRadioButton;
     private JRadioButton modificarRadioButton;
@@ -10,16 +12,49 @@ public class InstitutoForm {
     private JPanel ModificarPanel;
     private JPanel BuscarPanel;
     private JPanel EliminarPanel;
-    private JButton AGREGARButton;
-    private JTextField textField1;
-    private JTextField textField2;
+    private JButton AgregarButton;
+    private JTextField CodigoTextField;
+    private JTextField NombresTextField;
     private JRadioButton alFinalRadioButton;
     private JRadioButton posiciónRadioButton;
     private JRadioButton alInicioRadioButton;
-    private JTextField textField3;
+    private JTextField PosicionTextField;
     private JRadioButton porCódigoRadioButton;
     private JRadioButton porNombreRadioButton;
     private JTextField textField4;
     private JTextField textField5;
     private JButton BUSCARButton;
+    private JTextField modificarBuscarTextField;
+    private JTextField nuevoNombreTextField;
+    private JRadioButton porCódigoRadioButton1;
+    private JRadioButton porPosiciónRadioButton;
+    private JPanel EliminarCardPanel;
+    private JPanel CodigoPanel;
+    private JPanel PosicionPanel;
+    private JTextField EliminarCodigoTextField;
+    private JButton EliminarButton;
+    private JTextField textField3;
+    private JButton ELIMINARButton;
+    private JButton BUSCARModificarButton;
+    private JButton MODIFICARButton;
+    private JFrame jFRame = new JFrame();
+
+    public InstitutoForm() {
+        BUSCARModificarButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    int code = Integer.parseInt(modificarBuscarTextField.getText());
+                } catch (Exception exception) {
+                    JOptionPane.showMessageDialog(jFRame, "Ingrese un codigo valido");
+                }
+
+
+            }
+        });
+    }
+
+    private void createUIComponents() {
+
+    }
 }
