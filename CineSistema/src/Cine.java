16ebf503dace5a5674853ca18aacb92017bb8aac
damarizz <138645520+damarizz@users.noreply.github.com>
2023-11-17 10:10:49 -0500

    moson pullea altoque

---
+++ CineSistema/src/Cine.java
@@ -0,0 +1,8 @@
+import javax.swing.*;
+
+public class Cine {
+    private JPanel CinePanel;
+    private JRadioButton algorRadioButton;
+    private JRadioButton radioButton2;
+    private JRadioButton radioButton3;
+}
