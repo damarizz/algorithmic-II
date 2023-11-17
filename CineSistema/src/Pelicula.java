57aa86e0aa5be23d1f37a29b2fd9c2c73adea8c8
damarizz <138645520+damarizz@users.noreply.github.com>
2023-11-17 10:02:12 -0500

    feat: pelicula class atributes

---
+++ CineSistema/src/Pelicula.java
@@ -0,0 +1,18 @@
+import java.util.Date;
+
+public class Pelicula {
+    private int id;
+    private String tituloDistribucion;
+    private String tituloOriginal;
+    private String genero;
+    private String idiomaOriginal;
+    private boolean subtitulos;
+    private String pais;
+    private int añoProduccion;
+    private String sitioWeb;
+    private String duracion;
+    private String calificacion;
+    private Date fechaEstreno;
+    private String resumen;
+
+}
