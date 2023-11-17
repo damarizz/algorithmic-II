4eadff948c3cec6b509d24911b6e832c594bf94a
damarizz <138645520+damarizz@users.noreply.github.com>
2023-11-17 11:06:38 -0500

    feat: gui form

--- CineSistema/src/Cine.java
+++
@@ -1,8 +0,0 @@
-import javax.swing.*;
-
-public class Cine {
-    private JPanel CinePanel;
-    private JRadioButton algorRadioButton;
-    private JRadioButton radioButton2;
-    private JRadioButton radioButton3;
-}
